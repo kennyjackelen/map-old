6e00ed880c451f04475bfcd1355a0f8692ef4f10
Kenny Jackelen <kenny@kennyjackelen.com>
2013-04-18 21:12:26 -0500

    split js into separate file

---
+++ js/main.js
@@ -0,0 +1,71 @@
+var map;
+  
+function initialize() {
+	
+	var myOptions = {
+	  zoom: 4,
+	  center: new google.maps.LatLng(39, -92),
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	map = new google.maps.Map(document.getElementById("map_canvas"),
+		myOptions);
+		
+	placeMarkers();
+	
+}
+  
+function placeMarkers(){
+	
+	var cities = new Array();
+	cities = 
+	[
+		{ town:"Madison, WI", lat:43.067, lng:-89.4 },
+		{ town:"Eagan, MN", lat:44.817, lng:-93.167 },
+		{ town:"Minneapolis, MN", lat:44.967, lng:-93.25 },
+		{ town:"South Bend, IN", lat:41.667, lng:-86.25 },
+		{ town:"Chicago, IL", lat:41.833, lng:-87.683 },
+		{ town:"Milwaukee, WI", lat:43.05, lng:-87.95 },
+		{ town:"Waukesha, WI", lat:43.1, lng:-88.183 },
+		{ town:"Cleveland, OH", lat:41.467, lng:-81.667 },
+		{ town:"Cincinnati, OH", lat:39.133, lng:-84.5 },
+		{ town:"Philadelphia, PA", lat:39.953, lng:-75.17 },
+		{ town:"New York, NY", lat:40.717, lng:-74.0 },
+		{ town:"Boston, MA", lat:42.35, lng:-71.15 },
+		{ town:"Fairfield, CT", lat:41.167, lng:-73.267 },
+		{ town:"Seattle, WA", lat:47.6, lng:-122.317 },
+		{ town:"Washington, DC", lat:38.883, lng:-77.033 },
+		{ town:"Mesa, AZ", lat:33.4, lng:-111.817 },
+		{ town:"Los Angeles, CA", lat:34.2, lng:-118.25 },
+		{ town:"Fresno, CA", lat:36.733, lng:-119.767 },
+		{ town:"Colorado Springs, CO", lat:38.85, lng:-104.783 },
+		{ town:"Iowa City, IA", lat:41.65, lng:-91.517 },
+		{ town:"Bemidji, MN", lat:47.467, lng:-94.867 },
+		{ town:"Fargo, ND", lat:46.867, lng:-96.783 },
+		{ town:"Bismarck, ND", lat:46.813, lng:-100.779 },
+		{ town:"Duluth, MN", lat:46.787, lng:-92.098 },
+		{ town:"Kansas City, MO", lat:39.10, lng:-94.58 },
+		{ town:"Luverne, MN", lat:43.65, lng:-96.2 },
+		{ town:"San Juan, PR", lat:18.45, lng:-66.067 },
+    { town:"Omaha, NE", lat:41.25, lng:-96.0 },
+    { town:"Annapolis, MD", lat:38.967, lng:-76.5 },
+    { town:"Denver, CO", lat:39.739, lng:-104.984 },
+    { town:"Portland, OR", lat:45.52, lng:-122.682 },
+    { town:"New Haven, CT", lat:41.31, lng:-72.923611 },
+    { town:"Richland, WA", lat:46.279722, lng:-119.281389 },
+    { town:"Brainerd, MN", lat:46.358056, lng:-94.200833 },
+    { town:"St. Cloud, MN", lat:45.553889, lng:-94.170278 },
+    { town:"St. Louis, MO", lat:38.63, lng:-90.2 },
+    { town:"San Francisco, CA", lat:37.7793, lng:-122.4192 },
+    { town:"Lake Buena Vista, FL", lat:28.418611, lng:-81.581111 }
+	];
+	
+	for(var city in cities)
+	{
+		var marker = new google.maps.Marker({
+			map: map, 
+			position: new google.maps.LatLng(cities[city].lat, cities[city].lng),
+			title: cities[city].town
+		});
+	}
+}
